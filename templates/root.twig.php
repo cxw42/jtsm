78defdd7ba170f074e32f9a4af075fe87ed4feb0
cxw <cxwembedded@gmail.com>
2017-01-21 13:22:04 -0500

    Began converting this to a package.
    Removed files specific to a test and not required for the package itself.

--- templates/root.twig.php
+++
@@ -1,8 +0,0 @@
-{# <?php die();?> #}{# root: mainpage #}
-
-Howdy!
-
-Check out <a href="{{gen('search')}}">search!</a>
-
-{# vi: set ts=4 sts=4 sw=4 et ai ff=unix: #}
-
--- templates/user.twig.php
+++
@@ -1,8 +0,0 @@
-{# <?php die();?> #}
-
-User {{ id }} was here!
-
-Back to the <a href="{{gen('users')}}">Users list</a>
-
-{# vi: set ts=4 sts=4 sw=4 et ai ff=unix: #}
-
